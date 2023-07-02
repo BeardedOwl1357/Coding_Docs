c3d1858c910c71bbb7c50459cc9ffe5306cafa74
BeardedOwl1357 <sanchay.joshi2023@gmail.com>
2023-07-02 15:48:11 +0530

    Started learning Dart and making notes.

--- Flutter_and_Dart/Dart/variables.dart
+++ Flutter_and_Dart/Dart/variables.dart
@@ -9,4 +9,15 @@ void main() {
   print(x);
   x = 4;
   print(x);
+
+  // Finding absolute values of numbers
+  print((-5).abs()); // Int
+  print((-5.123).abs()); // Double
+
+  // Multiline String
+  String multi_line = """
+This is good
+Very good
+""";
+  print(multi_line);
 }
